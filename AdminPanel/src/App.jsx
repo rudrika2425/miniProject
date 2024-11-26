285fb16303cda30922c61b7f1e306268da9233dd
anushka2300 <122161237+anushka2300@users.noreply.github.com>
2024-11-26 10:09:08 +0530

    detailsPage

--- AdminPanel/src/App.jsx
+++ AdminPanel/src/App.jsx
@@ -4,19 +4,18 @@ import Page from './page';
 import AddProperty from './AddProperty/property'; 
 import UpdateProperty from './updateProperty/update'; 
 import UpdateRoom from './updateProperty/updateRoom'
-import { AuthProvider } from './Context';
+import Detail from './AddProperty/Detail'
 
 const App = () => {
   return (
-    <Router>
-      <AuthProvider>
+    <Router>    
       <Routes>
         <Route path="/" element={<AddProperty />} />
         <Route path="/addProperty/property" element={<AddProperty />} />
         <Route path="/updateProperty" element={<UpdateProperty />} />
         <Route path="/updateRoom" element={<UpdateRoom />} />
+        <Route path="/details" element={<Detail/>}/>
       </Routes>
-      </AuthProvider>
     </Router>
   );
 }
--- backend/models/AdminSchema.js
+++ backend/models/AdminSchema.js
@@ -19,6 +19,7 @@ const adminSchema= new mongoose.Schema({
             type:String,
             required:true,
         }
+        
 })
 
 const Admin=mongoose.model('Admin',adminSchema);
